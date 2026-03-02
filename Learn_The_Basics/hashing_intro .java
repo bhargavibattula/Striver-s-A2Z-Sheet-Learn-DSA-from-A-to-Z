84fef71974068f1f126ceb91cb29fd658d3a4946
Bhargavi Battula <137885457+bhargavibattula@users.noreply.github.com>
2026-03-02 23:53:58 +0530

    Create hashing_intro .java

---
+++ Learn_The_Basics/hashing_intro .java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<Integer>> countFrequencies(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<>();  
+
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1); 
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            result.add(Arrays.asList(entry.getKey(), entry.getValue())); 
+        }
+
+       
+        return result;
+    }
+} 
