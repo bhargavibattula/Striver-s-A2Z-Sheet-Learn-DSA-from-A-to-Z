62811b8192421af11423a68d76fd825d90851e90
Bhargavi Tejaswi <your-email@example.com>
2026-03-04 21:11:55 +0530

    rotate array , remove duplicates

---
+++ Arrays/easy/rotateArrayByOne.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void rotateArrayByOne(int[] nums) {
+        int n = nums.length;
+        int temp = nums[0];
+        for(int i =1; i < n ; i++)
+        {
+            nums[i-1] = nums[i];
+        }
+
+        nums[n-1] = temp;
+        
+    }
+}
